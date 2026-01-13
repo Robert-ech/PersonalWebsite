1b163ca61ab592833e7f6ddc275da6ccbd29c8fb
Roberto Echeverria <145717389+Robert-ech@users.noreply.github.com>
2026-01-13 11:41:32 -0800

    configured github files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your actual repo name
+  base: '/PersonalWebsite/', // Replace 'PersonalWebsite' with your actual repo name
 })
